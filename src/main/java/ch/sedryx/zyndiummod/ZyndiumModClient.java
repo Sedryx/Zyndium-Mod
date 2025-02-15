3eaf48c8cb3a5be0a6581ce9d12ff6a9c86835a2
joachim <joachim.espin@studentfr.ch>
2025-02-15 11:13:49 +0100

    Initial commit

---
+++ src/main/java/ch/sedryx/zyndiummod/ZyndiumModClient.java
@@ -0,0 +1,11 @@
+package ch.sedryx.zyndiummod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ZyndiumModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
